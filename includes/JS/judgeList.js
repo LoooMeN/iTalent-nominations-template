e05b2105b91e88b0c08f1f6c5eb9b27e29e55b51
unknown <looomen@hotmail.com>
2019-10-23 14:09:05 +0300

    Reorganized files

---
+++ includes/JS/judgeList.js
@@ -0,0 +1,204 @@
+var judgeList = [{
+    //name = имя
+    //descr = должность
+    //photo = ссылка на фото
+    //position: mentor / judge / all
+    //fb: ссылка на фб
+    //noms: номинации которые судит
+    'name': 'АШИРОВ АРТУР',
+    'descr': 'HEAD OF DEVELOPMENT, TECH LEAD У TAIN UKRAINE',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%B0%D1%88%D0%B8%D1%80%D0%BE%D0%B2.png',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['WEBAPP'],
+}, {
+    'name': 'ВАСЮК ТЕТЯНА',
+    'descr': 'АСИСТЕНТ ВИПУСКОВОГО РЕЖИСЕРА ІНФОРМАЦІЙНОЇ СЛУЖБИ «ФАКТИ», ICTV',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2019/04/%D0%A2%D0%B0%D1%82%D1%8C%D1%8F%D0%BD%D0%B0-%D0%92%D0%B0%D1%81%D1%8E%D0%BA.png',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['VID'],
+}, {
+    'name': 'ГЛУШКО БОГДАН',
+    'descr': 'CEO, Z-DIGITAL AGENCY',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/11/photo_2018-02-23_23-23-50-Bohdan-Glushko.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['PROG', 'WEBAPP', 'WEBFRONT', 'WEBCONST'],
+}, {
+    'name': 'ДОБРОВОЛЬСЬКА ЛАРИСА',
+    'descr': 'ВОЛОНТЕР ПРОЕКТУ CODECLUBUA, ВЧИТЕЛЬ ІНФОРМАТИКИ У ТЛУМАЦЬКІЙ ГІМНАЗІЇ',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%B4%D0%BE%D0%B1%D1%80%D0%BE%D0%B2%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['SCRATCH'],
+}, {
+    'name': 'ЄГЕРОВ СЕРГІЙ',
+    'descr': 'VFX PROJECT SUPERVISOR В ROOM8STUDIO. ВИКЛАДАЧ 3D-COAT ТА CONCEPT ART В GAMES ACADEMY',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D1%8D%D0%B3%D0%BE%D1%80%D0%BE%D0%B2.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['3D', 'GAMEDEV'],
+}, {
+    'name': 'ЄКИМЕНКО ОЛЕКСІЙ',
+    'descr': 'ВІДЕОБЛОГЕР, КІНОКРИТИК, АСИСТЕНТ РЕЖИСЕРА ІНФОРМАЦІЙНОЇ СЛУЖБИ «ФАКТИ»',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%B5%D0%BA%D0%B8%D0%BC%D0%B5%D0%BD%D0%BA%D0%BE.png',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['VID', 'PHOTO'],
+}, {
+    'name': 'ЄРЕМЕНКО АНТОН',
+    'descr': 'FOUNDER ITALENT',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%90%D0%BD%D1%82%D0%BE%D0%BD.png',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['PROG', 'PROGAPP'],
+}, {
+    'name': 'ІЗМАЙЛОВА МАРІЯ',
+    'descr': 'АРТ ДИРЕКТОР У КОМПАНІЇ NETENT',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%B8%D0%B7%D0%BC%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2%D0%B0.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['2DG', '2DA', 'PHOTO'],
+}, {
+    'name': 'КОЛОМІЄЦЬ ІВАН',
+    'descr': 'ФУЛСТЕК ВЕБ РОЗРОБНИК, ДИРЕКТОР З ІНФОРМАЦІЙНИХ ТЕХНОЛОГІЙ U-KNIGHT WEB STUDIO LLC',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/10/DSC_1016.NEF_tr.jpg',
+    'position': 'all',
+    'fb': '',
+    'noms': ['WEBAPP', 'WEBFRONT', 'WEBCONST'],
+}, {
+    'name': 'КОРНІЄНКО ДМИТРО',
+    'descr': 'КУРАТОР НАПРЯМКУ РОБОТОТЕХНІКА У BROBOTS',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2019/04/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%BE-%D0%9A%D0%BE%D1%80%D0%BD%D1%96%D1%94%D0%BD%D0%BA%D0%BE.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['PROG', 'PROGAPP', 'GAMEDEV'],
+}, {
+    'name': 'КРАВЦОВ СЕРГІЙ',
+    'descr': 'ЗАСНОВНИК EVERGREEN.IT DEVELOPMENT, SUPPORT & SERVICE',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%BA%D1%80%D0%B0%D0%B2%D1%86%D0%BE%D0%B2.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['PROGAPP'],
+}, {
+    'name': 'КРИВИЧ СВІТЛАНА',
+    'descr': 'КЕРІВНИК TAKE PRODUCTION',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/12/%D0%A1%D0%B2%D0%B5%D1%82%D0%BB%D0%B0%D0%BD%D0%B0-%D0%9A%D1%80%D0%B8%D0%B2%D0%B8%D1%87.jpeg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['VID', 'PHOTO'],
+}, {
+    'name': 'КУБРАК ІРИНА',
+    'descr': "ГРАФІЧНИЙ ДИЗАЙНЕР, ВИКЛАДАЧ КУРСІВ КОМП'ЮТЕРНА ГРАФІКА ТА ІНФОГРАФІКА",
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%BA%D1%83%D0%B1%D1%80%D0%B0%D0%BA.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['2DG'],
+}, {
+    'name': 'ЛЮБІНЕЦЬ ОКСАНА',
+    'descr': 'ВОЛОНТЕР ПРОЕКТУ CODE CLUB UA',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%BB%D1%8E%D0%B1%D0%B8%D0%BD%D0%B5%D1%86.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['SCRATCH'],
+}, {
+    'name': 'НЕЙМАН ЄВГЕН',
+    'descr': 'PHP ПРОГРАМІСТ, DREAMSCAPE NETWORKS',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D0%BD%D0%B5%D0%B9%D0%BC%D0%B0%D0%BD.jpg',
+    'position': 'all',
+    'fb': '',
+    'noms': ['PROG', 'PROGAPP', 'GAMEDEV'],
+}, {
+    'name': 'РИСЮК ОЛЕКСІЙ',
+    'descr': 'CONCEPT DESIGNER AT GSC',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2019/07/%D0%9E%D0%BB%D0%B5%D0%BA%D1%81%D1%96%D0%B9-%D0%A0%D0%B8%D1%81%D1%8E%D0%BA.gif',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['2DG', '3D', 'GAMEDEV'],
+}, {
+    'name': 'РУМЯНЦЕВ ОЛЕКСАНДР',
+    'descr': 'ЗАСНОВНИК OLEKSFILM PRODUCTION, ВІДЕОБЛОГЕР, ВИКЛАДАЧ ШКОЛИ ТЕЛЕЖУРНАЛІСТИКИ MEDIASTART.SCHOOL',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/10/%D0%A0%D1%83%D0%BC%D1%8F%D0%BD%D1%86%D0%B5%D0%B2.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['VID', 'PHOTO'],
+}, {
+    'name': 'САПРИКІНА ОЛЬГА',
+    'descr': "КЕРIВНИК КОМП'ЮТЕРНОГО КЛУБУ ZX, ЦПР СВЯТОШИНСЬКОГО РАЙОНУ",
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D1%81%D0%B0%D0%BF%D1%80%D0%B8%D0%BA%D0%B8%D0%BD%D0%B0.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['2DG', '2DA'],
+}, {
+    'name': 'ТЕБЕШЕВСЬКА МАРГАРИТА',
+    'descr': 'РЕЖИСЕР АНІМАЦІНИХ ФІЛЬМІВ, ВИКЛАДАЧ КИЇВСЬКОГО УНІВЕРСИТЕТУ ІМ. БОРИСА ГРІНЧЕКА, ЕФІРНИЙ ДИЗАЙНЕР HROMADSKE.UA',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D1%82%D0%B5%D0%B1%D0%B5%D1%88%D0%B5%D0%B2%D1%81%D1%8C%D0%BA%D0%B0.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['VID', '2DA'],
+}, {
+    'name': 'ФАРИСЕЙ КОСТЯНТИН',
+    'descr': 'QA СПЕЦІАЛІСТ',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2019/09/%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD-%D0%A4%D0%B0%D1%80%D0%B8%D1%81%D0%B5%D0%B9.gif',
+    'position': 'all',
+    'fb': '',
+    'noms': ['GAMEDEV'],
+}, {
+    'name': 'ТКАЧ ЛІЛІЯ',
+    'descr': 'LEAD ARTIST, WARGAMING',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/12/%D0%9B%D1%96%D0%BB%D1%96%D1%8F-%D0%A2%D0%BA%D0%B0%D1%87.jpg',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['3D'],
+}, {
+    'name': 'ШУБ РОДІОН',
+    'descr': 'КЕРІВНИК АНІМАЦІЙНОЇ СТУДІЇ RIDNI ANIMATION',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/09/%D1%88%D1%83%D0%B1.png',
+    'position': 'judge',
+    'fb': '',
+    'noms': ['2DG', '2DA', 'VID'],
+}, {
+    'name': 'МІНДРА ДМИТРО',
+    'descr': 'ENGINEERING MANAGER @ Grammarly',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/10/13407143_10209789719758071_5860870980547050263_n-Dmytro-Mindra.jpg',
+    'position': 'mentor',
+    'fb': '',
+    'noms': ['2DG', '2DA', 'VID'],
+}, {
+    'name': 'КАРКУНОВ ЯРОСЛАВ',
+    'descr': 'SOFTWARE ENGINEER, Grammarly Inc.',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/11/2018-06-11-18.39.11-Iaroslav-Karkunov.jpg',
+    'position': 'mentor',
+    'fb': '',
+    'noms': ['2DG', '2DA', 'VID'],
+}, {
+    'name': 'ЄЛЬЧАНІНОВ ДМИТРО',
+    'descr': 'Доцент кафедри інформатики та інтелектуальної власності Національного технічного університету «Харківський політехнічний інститут»',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2018/10/%D0%A4%D0%BE%D1%82%D0%BE-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9-%D0%95%D0%BB%D1%8C%D1%87%D0%B0%D0%BD%D0%B8%D0%BD%D0%BE%D0%B2.jpg',
+    'position': 'mentor',
+    'fb': '',
+    'noms': ['2DG', '2DA', 'VID'],
+}, {
+    'name': 'АНДРЮЩЕНКО НАДІЯ',
+    'descr': 'Співзасновниця  ІТ школи для дітей віком 5+ KidIT, Product Discovery Analyst в MacPaw',
+    'photo': 'https://italent.org.ua/wp-content/uploads/2019/10/DSC08177-Nadia-Andrushchenko.jpg',
+    'position': 'mentor',
+    'fb': '',
+    'noms': ['2DG', '2DA', 'VID'],
+}, ];
+
+function compare(a, b) {
+    if (a.name < b.name) {
+        return -1;
+    }
+    if (a.name > b.name) {
+        return 1;
+    }
+    return 0;
+}
+
+judgeList.sort(compare);
+export {
+    judgeList
+};
