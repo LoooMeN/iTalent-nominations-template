646cf58a40517157c33e299dabd3d4077b78ba49
LoooMeN <looomen@hotmail.com>
2020-02-04 17:49:00 +0200

    added burger

--- includes/JS/includeHTML.js
+++ includes/JS/includeHTML.js
@@ -1,13 +1,41 @@
+let settings;
+
+function set_hamburger_trigger() {
+    let burger = document.querySelector(".mobileMenuSwitch");
+    let mobileMenu = document.querySelector("#mobileMenu");
+    console.log(mobileMenu);
+
+    burger.addEventListener("click", function () {
+        if (mobileMenu.classList.contains("open")) {
+            mobileMenu.classList.remove("open");
+            mobileMenu.classList.add("closed");
+            burger.classList.remove("rotateLeft");
+            burger.classList.add("rotateRight");
+        } else {
+            mobileMenu.classList.remove("closed");
+            mobileMenu.classList.add("open");
+            if (burger.classList.contains("rotateRight"))
+                burger.classList.remove("rotateRight");
+            burger.classList.add("rotateLeft");
+        }
+    })
+}
+
+function get_settings() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "includes/data/settings.json", false);
+    request.send();
+    settings = JSON.parse(request.responseText);
+}
+
 function includeHTML() {
+    get_settings();
     var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
+    z = document.getElementsByTagName("div");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
         file = elmnt.getAttribute("w3-include-html");
         if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
@@ -17,15 +45,14 @@ function includeHTML() {
                     if (this.status == 404) {
                         elmnt.innerHTML = "Page not found.";
                     }
-                    /* Remove the attribute, and call this function once more: */
                     elmnt.removeAttribute("w3-include-html");
                     includeHTML();
                 }
             }
             xhttp.open("GET", file, true);
             xhttp.send();
-            /* Exit the function: */
             return;
         }
     }
+    window.onload = set_hamburger_trigger();
 }
