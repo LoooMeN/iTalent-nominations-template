412674308f718aeda30ae60e578d0db11b977807
LoooMeN <looomen@hotmail.com>
2020-02-19 16:46:00 +0200

    fix

---
+++ nominations/gamedev.php
@@ -0,0 +1,154 @@
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <link rel='stylesheet' href='/includes/CSS/nominationStyle.css'>
+    <?php include '../includes/header.html';?>
+    <script src='/includes/JS/youtube.js'></script>
+    <title>GAMEDEV</title>
+</head><body>
+    <script>
+        var page = 'gamedev';
+    </script>
+    <section class='section' id='aboutNomination'>
+    <h1 class='title'>GAMEDEV</h1>
+    <p>У цій номінації вам потрібно зробити свою гру, надати трейлер та туторіал. Уважно читайте вимоги до гри.</p>
+</section>
+
+    <section id='ageGroups' class='section'>
+        <div class='ageGroupsWrapper'>
+            <div class='ageGroupWrapper shadow'>
+                <div class='ageGroupPreview'>
+                    <img src='/includes/images/backgrounds/GameAgeBg_1.png'
+                        alt='Вікова категорія 1'>
+                    <div class='age blueGrad'>
+                        <h2>Від 8 до 15 років</h2>
+                        <p>Тема роботи: вільна</p>
+                    </div>
+                </div></div><div class='ageGroupWrapper shadow'>
+        <div class='ageGroupPreview'>
+            <img src='/includes/images/backgrounds/GameAgeBg_2.png'
+                alt='Вікова категорія 1'>
+            <div class='age redGrad'>
+                <h2>Від 15 до 19 років</h2>
+                <p>Тема роботи: вільна</p>
+            </div>
+        </div></div>        </div>
+    <p>*Допускається розробка та подача проектів у складі команди, проте зауважте, що у разі
+        перемоги - одна робота отримує одну нагороду.</p>
+    </section>
+    <div class='kriteriiBg'>
+    <section id='kriterii' class='section'>
+        <h1 class='title'>Критерії за якими будуть<br>оцінювати вашу роботу</h1>
+        <div class='kriterii'>
+            <div class='innerWrapper shadow'>
+                <img src='/includes/images/icons/ConfetiIcon.svg' alt='icon'>
+                <h2>FUN</h2>
+                Цікавість сюжетної історії, оригінальність ідеї гри, креативність рішення. Наскільки цікаво грати в гру.</div>
+            <div class='innerWrapper shadow'>
+                <img src='/includes/images/icons/puzzle.svg' alt='icon'>
+                <h2>GAMEDESIGN</h2>
+                Стиль, якість виконання, наскільки приємно дивитись на гру. Зручність управління та навігації, наявність підказок.</div>
+            <div class='innerWrapper shadow'>
+                <img src='/includes/images/icons/listIcon.svg' alt='icon'>
+                <h2>COMPLETENESS</h2>
+                Наявність повноцінного (без багів) геймплею з точки зору задуму автора і жанру.</div>
+            <div class='innerWrapper shadow'>
+                <img src='/includes/images/icons/SoundIcon.svg' alt='icon'>
+                <h2>АУДІО - СУПРОВІД</h2>
+                Якість музичного супроводу та його відповідність сюжету гри</div>
+            <div class='innerWrapper shadow'>
+                <img src='/includes/images/icons/eye.svg' alt='icon'>
+                <h2>VISUAL</h2>
+                Якість графіки. Дотримання обраної стилістики гри. Використання власноручно зроблених ігрових ассетів.</div>
+            <div class='innerWrapper shadow'>
+                <img src='/includes/images/icons/monitor.svg' alt='icon'>
+                <h2>TECHNICAL</h2>
+                Наскільки використовуються засоби програмування, кількість та складність алгоритмів; використання засобів та технологій: робота з мережею, з базами даних, ввід/вивід даних, логування і т.і.</div>
+            <div class='innerWrapper shadow'>
+                <img src='/includes/images/icons/mark.svg' alt='icon'>
+                <h2>ВІДПОВІДНІСТЬ</h2>
+                Умовам конкурсу та вимогам до робіт. Роботи, що не відповідають вимогам – розглядитись суддями не будуть.</div>
+        </div>
+    </section>
+</div>
+    <div class='vimogiBg'>
+        <section class='section' id='vimogi'>
+            <h1 class='title'>Вимоги до<br>вашої роботи:</h1>
+            <div class='vimogi'>
+    <div class='vimogaWrapper'>
+                    <img class='icon' src='/includes/images/icons/BoxIcon.svg' alt='icon'>
+                    <div class='text'>
+                        <h3>Архів із грою</h3>
+                        <p>Папку із файлами запуску гри розміром не більше 5 гб. Якщо гра не запускається із цієї папки – робота розглядатись не буде.</p>
+                    </div>
+                </div><div class='vimogaWrapper'>
+                    <img class='icon' src='/includes/images/icons/GraphicsIcon.svg' alt='icon'>
+                    <div class='text'>
+                        <h3>Tutorial</h3>
+                        <p>Вбудована в гру інструкція про те як в неї грати. Наприклад: відео інструкція на початку гри (із можливістю пропустити відео), окремий навчальний рівень, частина першого рівня і т.д.</p>
+                    </div>
+                </div><div class='vimogaWrapper'>
+                    <img class='icon' src='/includes/images/icons/Camera.svg' alt='icon'>
+                    <div class='text'>
+                        <h3>Необхідно зробити</h3>
+                        <p>відео-огляд своєї гри, тривалістю до 5 хвилин. В цьому відео показати:</p><ul><li>Геймплей (сам ігровий процесс)</li><li>Ігрові механіки (пояснити їх)</li><li>Концепт, сюжет гри</li></ul><p>Відео необхідно завантажити на Youtube та надати посилання, доступ до нього має бути обмежений для перегляду лише за посиланням</p>
+                    </div>
+                </div><div class='vimogaWrapper'>
+                    <img class='icon' src='/includes/images/icons/monitor.svg' alt='icon'>
+                    <div class='text'>
+                        <h3>Якщо в грі</h3>
+                        <p>є більше одного рівня – необхідно передбачити вибір рівня в меню гри (це необхідно щоб суддям не довелося проходити всі рівні до кінця, щоб оцінити роботу)</p>
+                    </div>
+                </div><div class='vimogaWrapper'>
+                    <img class='icon' src='/includes/images/icons/BoxIcon.svg' alt='icon'>
+                    <div class='text'>
+                        <h3>Архів із проектом гри</h3>
+                        <p>Окремий архів із усіма вихідними файлами гри</p>
+                    </div>
+                </div><div class='vimogaWrapper'>
+                    <img class='icon' src='/includes/images/icons/BoxIcon.svg' alt='icon'>
+                    <div class='text'>
+                        <h3>Архів з роботою</h3>
+                        <p>слід називати ім’ям конкурсанта. Приклад: Ivahin_Ivan.zip</p> <p> обов’ язково заархівувати саме у “zip” формат, а не “rar” чи інший. </p> <p> архів необхідно завантажити на онлайн сховище GoogleDrive.</p> <p> УВАГА!Не забудьте надати доступ до архіву. </p><a href = '/includes/images/archive_instructions.jpg' target = '_blank'><div class = 'button shadow'>ДЕТАЛЬНА ІНСТРУКЦІЯ ТУТ </div> </a>
+                    </div>
+                </div><div class='vimogaWrapper'>
+                    <img class='icon' src='/includes/images/icons/mark.svg' alt='icon'>
+                    <div class='text'>
+                        <h3>Відповідність</h3>
+                        <a href='/project-demands/' target='_blank'><div class = 'button shadow'> ЗАГАЛЬНИМ ВИМОГАМ ДО РОБІТ </div> </a>
+                    </div>
+                </div></div>
+    </section>
+</div><section class='section' id='registration'></section>
+
+<section id='CTAbuttons' class='section'>
+    <a href='http://winners.italent.org.ua/' class='CTAbutton shadow unselectable'>
+        <p>роботи попередніх сезонів</p>
+        <img class='buttonImg' src='/includes/images/icons/Vector-3.png'>
+    </a>
+    <a href='/mentorship/' class='CTAbutton shadow unselectable'>
+        <p>потрібна допомога с проектом?</p>
+        <img class='buttonImg' src='/includes/images/icons/Vector-3.png'>
+    </a>
+</section>
+
+<section id='judgeList' class='section'>
+    <h1 class='title'>СУДДІ НОМІНАЦІЇ</h1>
+    <div class='grod' id='judgeGrod'>
+        <script type='module' src='/includes/JS/judges.js'></script>
+    </div>
+    <a href='/judges/'>
+        <div class='judgeButton shadow'>усі судді</div>
+    </a>
+</section>
+
+<?php include '../includes/footer.html';?>
+
+</body>
+
+</html>
