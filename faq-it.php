60c85dbfb0f5bc26f4276b011a4f1551ef14a206
LoooMeN <looomen@hotmail.com>
2020-02-20 11:53:31 +0200

    Small content fixes

--- faq-it.php
+++ faq-it.php
@@ -35,7 +35,7 @@
             </div>
             <div class="red questionBox shadow">
                 <h2>Яка дата кінця реєстрації?</h2>
-                <p>1.12.2019 (дата може змінитися)</p>
+                <p>24 квітня 2020 (дата може змінитися)</p>
             </div>
             <div class="green questionBox bigBox shadow">
                 <h2>На фінал потрібно їхати за свій рахунок?</h2>
